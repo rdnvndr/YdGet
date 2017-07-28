239e18a5f8e775eb62261aafe9c1dbffbb9fef3b
Andrey Rodionov <roand@inbox.ru>
2017-07-28 14:34:22 +0300

    Change a getting of a file name from a link

--- background.js
+++ background.js
@@ -12,7 +12,6 @@ function showMessage(title, message) {
 
 function getStatus(href, token, link) {
     var request = new XMLHttpRequest();
-
     request.open("GET", href);
     request.setRequestHeader('Accept', 'application/json');
     request.setRequestHeader('Content-Type', 'application/json');
@@ -32,32 +31,49 @@ function getStatus(href, token, link) {
     request.send();
 };
 
+function getFilename(url){
+    var result = url.substring(0, (url.indexOf("#") == -1)
+                               ? url.length : url.indexOf("#"));
+    result = result.substring(0, (result.indexOf("?") == -1)
+                              ? result.length : result.indexOf("?"));
+    result = result.substring(result.lastIndexOf("/") + 1, result.length);
+
+    if (result == "") {
+        result = url.replace("http://","").replace("https://","").replace("/","");
+        result += ".html";
+    }
+    if (!(result.indexOf(".")+1))
+        result += ".html";
+
+    return result;
+}
+
 function getClickHandler() {
     return function(info, tab) {
         chrome.storage.sync.get({"token":"", "path":"disk:/Downloads"}, function(items) {
             if (!chrome.runtime.error) {
                 var token = items.token;
                 var link = info.linkUrl != null ? info.linkUrl : info.srcUrl;
-                var filename = link.lastIndexOf("/") > -1
-                        ? link.substr(link.lastIndexOf("/") + 1) : "";
-                if (filename == "") {
-                    filename = link.replace("http://","")
-                    .replace("https://","").replace("/","") + ".html";
-                }
-                var url = encodeURIComponent(link);
 
-                var path = encodeURIComponent(items.path + "/" + filename);
+                var url = encodeURIComponent(link);
+                var path = encodeURIComponent(
+                            items.path + "/" + getFilename(link));
 
                 var request = new XMLHttpRequest();
-
-                request.open("POST", 'https://cloud-api.yandex.net:443/v1/disk/resources/upload?disable_redirects=false'+"&url=" + url + "&path=" + path);
+                request.open("POST", 'https://cloud-api.yandex.net:443'
+                             +'/v1/disk/resources/upload?disable_redirects=false'
+                             +"&url=" + url + "&path=" + path);
                 request.setRequestHeader('Accept', 'application/json');
                 request.setRequestHeader('Content-Type', 'application/json');
                 request.setRequestHeader('Authorization', 'OAuth ' + token);
-
                 request.onload = function() {
-                    var obj = JSON.parse(request.responseText);
-                    getStatus(obj.href, token, link);
+                    if (request.status == 202) {
+                        var obj = JSON.parse(request.responseText);
+                        getStatus(obj.href, token, link);
+                    } else {
+                        showMessage(chrome.i18n.getMessage("downloadFailure"),
+                                    request.statusText);
+                    }
                 }
                 request.send();
             }
